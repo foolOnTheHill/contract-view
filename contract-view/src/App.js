b24085e70cc703ab7328f25a75ce053d2b601e4a
George Oliveira <ghao@cin.ufpe.br>
2017-05-17 20:52:27 -0300

    adding boilerplate

---
+++ contract-view/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './assets/bootstrap/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1>SUATI Contracts View</h1>
+          <p>Under development...</p>
+          <p><a className="btn btn-primary btn-lg" role="button">{"I\'m a button"}</a></p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ contract-view/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import contracts from './assets/mock-data/contracts.json'; // Fake data to test the app
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
